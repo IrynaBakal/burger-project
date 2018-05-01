b4ca9871af331a9a900a79f3133e92509f11b2c7
IrynaBakal <iriskaba@mail.ru>
2018-05-01 14:30:03 +0300

    added total sum to order summary

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -19,6 +19,7 @@ const orderSummary = (props) => {
 			<ul>
 				{ingredientSummary}
 			</ul>
+			<p><strong>Total sum: {props.totalSum.toFixed(2)}</strong></p>
 			<p>Continue to Checkout?</p>
 			<Button 
 				btnType='Danger'
